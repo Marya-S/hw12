ea5a0b1f3791766c18059fa2c903b1d3e77df6d4
Mary <7333581>
2022-07-20 22:53:51 +0300

    ДЗ 3

---
+++ src/com/company/Truck.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Truck extends Transport{
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
